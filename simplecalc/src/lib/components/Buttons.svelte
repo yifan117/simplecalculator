ef0922013b2e621c9b4e6d824d47cf1243e998f4
yifan117 <yifan.qiu17@gmail.com>
2022-08-27 21:38:27 +0930

    REMOVED REPEATED CODE IN BUTTONS

--- simplecalc/src/lib/components/Buttons.svelte
+++ simplecalc/src/lib/components/Buttons.svelte
@@ -9,65 +9,40 @@
 </script>
 
 <div class="calcbody">
-    <div class="row4">
-        <div class="clearsign">C</div>
-        <div class="changepl">+/-</div>
-        <div class="percent">%</div>
-        <div class="dividesign">÷</div>
+    <div class="row">
+        <div class="operationTop">C</div>
+        <div class="operationTop">+/-</div>
+        <div class="operationTop">%</div>
+        <div class="operationSign">÷</div>
     </div>
-    <div class="row3">
+    <div class="row">
         <div class="digits">7</div>
         <div class="digits">8</div>
         <div class="digits">9</div>
-        <div class="multiplysign">×</div>
+        <div class="operationSign">×</div>
     </div>
-    <div class="row2">
+    <div class="row">
         <div class="digits">4</div>
         <div class="digits">5</div>
         <div class="digits">6</div>
-        <div class="minussign">-</div>
+        <div class="operationSign">-</div>
     </div>
-    <div class="row1">
+    <div class="row">
         <div class="digits">1</div>
         <div class="digits">2</div>
         <div class="digits">3</div>
-        <div class="plussign">+</div>
+        <div class="operationSign">+</div>
     </div>
-    <div class="row0">
+    <div class="row">
         <div class="zero">0</div>
-        <div class="decimal">.</div>
-        <div class="equalsign">=</div>
+        <div class="digits">.</div>
+        <div class="operationSign">=</div>
     </div>
 </div>
 
 <style>
 
-.row0 {
-    gap: 8px;
-    padding: 3px;
-    display: flex;
-}
-
-.row1 {
-    gap: 8px;
-    padding: 3px;
-    display: flex;
-
-}
-
-.row2 {
-    gap: 8px;
-    padding: 3px;
-    display: flex;
-}
-
-.row3 {
-    gap: 8px;
-    padding: 3px;
-    display: flex;
-}
-
-.row4 {
+.row {
     gap: 8px;
     padding: 3px;
     display: flex;
@@ -117,28 +92,7 @@
     cursor: pointer;
 }
 
-.decimal {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #555555;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-    color: #ffffff;
-}
-
-.decimal:hover {
-    background-color: #ffffff;
-    opacity: 55%;
-    color: #000000;
-    cursor: pointer;
-}
-
-.equalsign {
+.operationSign {
     width: 45px;
     height: 45px;
     font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -152,98 +106,14 @@
     color: #ffffff;
 }
 
-.equalsign:hover {
+.operationSign:hover {
     background-color: #f5c77e;
     opacity: 80%;
     color: #000000;
     cursor: pointer;
 }
 
-.plussign {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #ec9006;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-    color: #ffffff;
-}
-
-.plussign:hover {
-    background-color: #f5c77e;
-    opacity: 80%;
-    color: #000000;
-    cursor: pointer;
-}
-
-.minussign {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #ec9006;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-    color: #ffffff;
-}
-
-.minussign:hover {
-    background-color: #f5c77e;
-    opacity: 80%;
-    color: #000000;
-    cursor: pointer;
-}
-
-.multiplysign {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #ec9006;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-    color: #ffffff;
-}
-
-.multiplysign:hover {
-    background-color: #f5c77e;
-    opacity: 80%;
-    color: #000000;
-    cursor: pointer;
-}
-
-.dividesign {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #ec9006;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-    color: #ffffff;
-}
-
-.dividesign:hover {
-    background-color: #f5c77e;
-    opacity: 80%;
-    color: #000000;
-    cursor: pointer;
-}
-
-.percent {
+.operationTop {
     width: 45px;
     height: 45px;
     font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
@@ -256,47 +126,7 @@
     border-radius: 40px;
 }
 
-.percent:hover {
-    background-color: #888888;
-    opacity: 80%;
-    color: #ffffff;
-    cursor: pointer;
-}
-
-.changepl {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #A9A9A9;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-}
-
-.changepl:hover {
-    background-color: #888888;
-    opacity: 80%;
-    color: #ffffff;
-    cursor: pointer;
-}
-
-.clearsign {
-    width: 45px;
-    height: 45px;
-    font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-    padding: 3px;
-    text-align: center;
-    background-color: #A9A9A9;
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    border-radius: 40px;
-}
-
-.clearsign:hover {
+.operationTop:hover {
     background-color: #888888;
     opacity: 80%;
     color: #ffffff;
